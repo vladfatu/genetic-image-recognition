c16782f842cb986fb29409cf1093b92722ff4a0b
vladfatu <colegu123@gmail.com>
2015-12-12 13:50:51 +0000

    -

---
+++ src/com/gir/GirTest.java
@@ -0,0 +1,7 @@
+package com.gir;
+
+/**
+ * Created by Vlad on 12-Dec-15.
+ */
+public class GirTest {
+}
---
+++ src/com/gir/ga/FitnessCalculator.java
@@ -0,0 +1,7 @@
+package com.gir.ga;
+
+/**
+ * Created by Vlad on 12-Dec-15.
+ */
+public class FitnessCalculator {
+}
